02207677591b727d6bffe9660f2da5e372562db6
J-C <jcjoneskern@gmail.com>
2017-01-05 01:18:02 -0500

    shows tests

---
+++ docs/spec/spec.js
@@ -0,0 +1,22 @@
+// var calc = require("../script.js");
+
+describe("calculation", function() {
+  it("calculates bedtime @8 for max time", function() {
+    expect(calc(1700, 2000, 400)).toEqual(132);
+  });
+  it("rounds to full hours", function() {
+    expect(calc(1730, 2030, 330)).toEqual(132);
+  });
+  it("handles leaving before midnight", function() {
+    expect(calc(1730, 2030, 2200)).toEqual(52);
+  });
+  it("handles leaving at midnight && midnight is 0", function() {
+    expect(calc(1730, 2030, 0)).toEqual(68);
+  });
+  it("handles leaving at midnight && midnight is 24", function() {
+    expect(calc(1730, 2030, 2400)).toEqual(68);
+  });
+  it("handles bedtime being before babysitter leaves", function() {
+    expect(calc(1730, 2100, 2000)).toEqual(36);
+  });
+});
