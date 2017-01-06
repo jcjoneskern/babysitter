0061fbff9a3a10a237ee83947727ad7130d21056
J-C <jcjoneskern@gmail.com>
2017-01-06 14:54:24 -0500

    test passing

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ module.exports = calc;
 
 //uses military time
 //assumes bedtime will always be before midnight
-//assumes babysitter will always arrive before bedtime
 function calc(start, bed, leave) {
-  var pay, wage;
+  var pay;
+  var wage = 12; //in 2 of 3 scenarios, wage starts at 12--saves a line
 
   //conversion in case end time is entered as 0000 rather than 2400
   if(leave == 0) {
@@ -12,23 +12,21 @@ function calc(start, bed, leave) {
   }
 
   if (bed > leave && leave > 500) { //if babysitter leaves before bedtime
-    wage = 12;
     pay = (Math.ceil((leave - start)/100))*wage;
-  } else if (start > bed) {
+  } else if (start > bed) { //if babysitter arrives after bedtime
     wage = 8;
     if(leave >= 0 && leave <= 400) {
-      pay = (Math.ceil(24 - (bed/100)))*wage;
+      pay = (Math.ceil(24 - (start/100)))*wage;
     } else {
-      pay += (Math.ceil((leave - bed)/100))*wage;
+      pay += (Math.ceil((leave - start)/100))*wage;
     }
     //midnight to end
     wage = 16;
     if(leave > 0 && leave <= 400) {
       pay += Math.ceil(leave/100)*wage;
     }
-  } else {
+  } else { //the most generic situation
     //start to bedtime
-    wage = 12;
     pay = (Math.ceil((bed - start)/100))*wage;
     //bedtime to midnight
     wage = 8;
@@ -45,16 +43,9 @@ function calc(start, bed, leave) {
   }
   return pay;
 }
-
-// The babysitter
-// - starts no earlier than 5:00PM
-// - leaves no later than 4:00AM
-// - gets paid $12/hour from start-time to bedtime
-// - gets paid $8/hour from bedtime to midnight
-// - gets paid $16/hour from midnight to end of job
-// - gets paid for full hours (no fractional hours)
-//
-// Feature:
-// As a babysitter
-// In order to get paid for 1 night of work
-// I want to calculate my nightly charge
+// Logic/potential scenarios:
+// Expected scenario: sitter arrives before bedtime and leaves after midnight
+// Other possible scenarios:
+// --sitter leaves before bedtime
+// --sitter leaves before midnight
+// --sitter arrives after bedtime
