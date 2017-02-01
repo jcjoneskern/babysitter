02c335b207f1d6e671fc66787d36285d7be0c880
J-C <jcjoneskern@gmail.com>
2017-01-31 21:01:21 -0500

    some refactoring

--- generic.js
+++ generic.js
@@ -1,14 +1,12 @@
 module.exports = generic;
 
 function generic(start, bed, leave) {
-  var pay;
-  var wage = 12;
-
   if(leave == 0) {
     leave = 2400;
   }
 
-  pay = (Math.ceil((bed - start)/100))*wage;
+  var wage = 12;
+  var pay = (Math.ceil((bed - start)/100))*wage;
   wage = 8;
   if(leave >= 0 && leave <= 400) {
     pay += (Math.ceil(24 - (bed/100)))*wage;
